876731ee05c89d0c94db8fea7f99928398267f63
Jorge Luis Paredes <paredesjorgeluis@gmail.com>
2024-04-08 19:21:11 +0200

    Mostrar listado de héroes

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -9,9 +9,10 @@ import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
 import { ListPageComponent } from './pages/list-page/list-page.component';
 import { NewPageComponent } from './pages/new-page/new-page.component';
 import { SearchPageComponent } from './pages/search-page/search-page.component';
+import { CardComponent } from './components/card/card.component';
 
 @NgModule({
-	declarations: [HeroPageComponent, LayoutPageComponent, ListPageComponent, NewPageComponent, SearchPageComponent],
+	declarations: [HeroPageComponent, LayoutPageComponent, ListPageComponent, NewPageComponent, SearchPageComponent, CardComponent],
 	imports: [CommonModule, HeroesRoutingModule, MaterialModule],
 })
 export class HeroesModule {}
