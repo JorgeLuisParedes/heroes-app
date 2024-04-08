c7f25d302b6c6fe31b54f341c0a2995c3d542f54
Jorge Luis Paredes <paredesjorgeluis@gmail.com>
2024-04-08 19:39:42 +0200

    HeroImage Pipe

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -10,9 +10,10 @@ import { ListPageComponent } from './pages/list-page/list-page.component';
 import { NewPageComponent } from './pages/new-page/new-page.component';
 import { SearchPageComponent } from './pages/search-page/search-page.component';
 import { CardComponent } from './components/card/card.component';
+import { HeroImagePipe } from './pipes/hero-image.pipe';
 
 @NgModule({
-	declarations: [HeroPageComponent, LayoutPageComponent, ListPageComponent, NewPageComponent, SearchPageComponent, CardComponent],
+	declarations: [HeroPageComponent, LayoutPageComponent, ListPageComponent, NewPageComponent, SearchPageComponent, CardComponent, HeroImagePipe],
 	imports: [CommonModule, HeroesRoutingModule, MaterialModule],
 })
 export class HeroesModule {}
