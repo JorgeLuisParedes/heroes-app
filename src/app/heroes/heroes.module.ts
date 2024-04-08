391fe67d999ebfbbe07426c91d6f2621a25931ef
Jorge Luis Paredes <paredesjorgeluis@gmail.com>
2024-04-09 00:58:55 +0200

    Angular Material - Autocomplete

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { HeroesRoutingModule } from './heroes-routing.module';
 import { MaterialModule } from '../material/material.module';
@@ -14,6 +15,6 @@ import { HeroImagePipe } from './pipes/hero-image.pipe';
 
 @NgModule({
 	declarations: [HeroPageComponent, LayoutPageComponent, ListPageComponent, NewPageComponent, SearchPageComponent, CardComponent, HeroImagePipe],
-	imports: [CommonModule, HeroesRoutingModule, MaterialModule],
+	imports: [CommonModule, HeroesRoutingModule, MaterialModule, ReactiveFormsModule],
 })
 export class HeroesModule {}
--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -3,7 +3,6 @@ import { HttpClient } from '@angular/common/http';
 import { Observable, catchError, of } from 'rxjs';
 import { Hero } from '../interfaces/hero.interface';
 import { environments } from '../../../environments/environments';
-
 @Injectable({ providedIn: 'root' })
 export class HeroesService {
 	private baseUrl: string = environments.baseUrl;
@@ -16,5 +15,9 @@ export class HeroesService {
 
 	getHeroById(id: string): Observable<Hero | undefined> {
 		return this.http.get<Hero>(`${this.baseUrl}/heroes/${id}`).pipe(catchError((error) => of(undefined)));
+	}
+
+	getSuggestions(query: string): Observable<Hero[]> {
+		return this.http.get<Hero[]>(`${this.baseUrl}/heroes?q=${query}&_limit=6`);
 	}
 }
