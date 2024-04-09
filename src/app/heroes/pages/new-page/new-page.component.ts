e09036eb3e6beef87e3c552c2b92cf2af10d2798
Jorge Luis Paredes <paredesjorgeluis@gmail.com>
2024-04-09 10:56:47 +0200

    Actualizar y crear héroes

--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -33,6 +33,14 @@ export class NewPageComponent {
 	onSubmit(): void {
 		if (this.heroForm.invalid) return;
 
-		// this.heroesService.updateHero(this.heroForm.value);
+		if (this.currentHero.id) {
+			this.heroesService.updateHero(this.currentHero).subscribe((hero) => {
+				// TODO: mostrar snackbar
+			});
+			return;
+		}
+		this.heroesService.addHero(this.currentHero).subscribe((hero) => {
+			// TODO: mostrar snackbar, y agregar a /heroes/edit hero.id
+		});
 	}
 }
